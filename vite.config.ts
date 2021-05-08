6aeb1cf27a5acfe7dabc33148720af1d04a45028
Andres Celis <adwher@outlook.com>
2021-05-08 13:26:30 -0500

    chore: change dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,13 @@ import { resolve } from "path"
 // plugins
 
 import vue from "@vitejs/plugin-vue"
-import markdown from "vite-plugin-md"
-import components from "vite-plugin-components"
 
 export default defineConfig({
     resolve: {
         alias: {
             "~/": resolve(__dirname, "./src"),
 
+            "@components": resolve(__dirname, "./src/components"),
             "@models": resolve(__dirname, "./src/models"),
             "@services": resolve(__dirname, "./src/services"),
             "@useCases": resolve(__dirname, "./src/useCases"),
@@ -27,13 +26,6 @@ export default defineConfig({
                     isCustomElement: tag => tag === "feather-icon"
                 }
             }
-        }),
-
-        markdown(),
-
-        components({
-            extensions: ["vue"],
-            dirs: ["src/components"],
         })
     ]
 })
